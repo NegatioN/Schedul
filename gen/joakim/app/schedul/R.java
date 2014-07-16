98c2fce2ba53a28f7343e9d5a81d886ee9b858ab
NegatioN <Joakimrishaug@gmail.com>
2014-07-16 20:15:07 +0200

    preview-pictures of current build

--- gen/joakim/app/schedul/R.java
+++ gen/joakim/app/schedul/R.java
@@ -17,6 +17,9 @@ public final class R {
     }
     public static final class color {
         public static final int lightRed=0x7f060000;
+        public static final int medium=0x7f060002;
+        public static final int unimportant=0x7f060003;
+        public static final int urgent=0x7f060001;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
